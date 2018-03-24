2b48006e188d0929558c6f9db215c24a9d4351ee
Suraj Shukla <surajshukla1105@gmail.com>
2018-03-25 02:25:22 +0800

    Add files via upload

---
+++ Sentiment Analysis.py
@@ -0,0 +1,80 @@
+import os
+from textblob import TextBlob
+import tweepy
+from tweepy import OAuthHandler
+import re
+
+
+
+class TwitterClient(object):
+    
+    def __init__(self, query, retweets_only=False, with_sentiment=False):
+        # You can get this from twitter dev app
+        consumer_key = os.environ['CONSUMER_KEY'] #Your consumer_key Key 
+        consumer_secret = os.environ['CONSUMER_SECRET'] #
+        access_token = os.environ['ACCESS_TOKEN']  # Your Access token
+        access_token_secret = os.environ['ACCESS_TOKEN_SECRET'] #
+        # Attempt authentication
+        try:
+            self.auth = OAuthHandler(consumer_key, consumer_secret)
+            self.auth.set_access_token(access_token, access_token_secret)
+            self.query = query
+            self.retweets_only = retweets_only
+            self.with_sentiment = with_sentiment
+            self.api = tweepy.API(self.auth)
+            self.tweet_count_max = 100  # Because of limit set by twitter
+        except:
+            print("Error in Authentication")
+
+    def set_query(self, query=''):
+        self.query = query
+
+    def set_with_sentiment(self, with_sentiment='false'):
+        self.with_sentiment = with_sentiment
+
+    def clean_tweet(self, tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+    def set_retweet_checking(self, retweets_only='false'):
+        self.retweets_only = retweets_only
+		
+		
+    def get_tweet_sentiment(self, tweet):
+        analysis = TextBlob(self.clean_tweet(tweet))
+        if analysis.sentiment.polarity > 0:
+            return 'positive'
+        elif analysis.sentiment.polarity == 0:
+            return 'neutral'
+        else:
+            return 'negative'
+
+    def get_tweets(self):
+        tweets = []
+
+        try:
+            recd_tweets = self.api.search(q=self.query,
+                                          count=self.tweet_count_max)
+            if not recd_tweets:
+                pass
+            for tweet in recd_tweets:
+                parsed_tweet = {}
+
+                parsed_tweet['text'] = tweet.text
+                parsed_tweet['user'] = tweet.user.screen_name
+                
+                if self.with_sentiment == 1:
+                    parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+                else:
+                    parsed_tweet['sentiment'] = 'unavailable'
+
+                if tweet.retweet_count > 0 and self.retweets_only == 1:
+                    if parsed_tweet not in tweets:
+                        tweets.append(parsed_tweet)
+                elif not self.retweets_only:
+                    if parsed_tweet not in tweets:
+                        tweets.append(parsed_tweet)
+
+            return tweets
+
+        except tweepy.TweepError as e:
+            print("Error : " + str(e))
